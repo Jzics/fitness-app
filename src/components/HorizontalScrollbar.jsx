5148bcfb41ce057047ca556105d9a4945e039d39
Jzics <jamesisaac87@gmail.com>
2023-05-10 12:19:58 +0100

    Complete project file

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -3,30 +3,31 @@ import { Box, Typography } from "@mui/material";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import BodyPart from "./BodyPart";
 import { useContext } from "react";
-import RightArrowIcon from '../assets/icons/right-arrow.png'
-import LeftArrowIcon from '../assets/icons/left-arrow.png'
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+import ExerciseCard from "./ExerciseCard";
 
 const LeftArrow = () => {
-    const {scrollPrev} = useContext(VisibilityContext);
+  const { scrollPrev } = useContext(VisibilityContext);
 
-    return (
-        <Typography onClick={() => scrollPrev()} className="right-arrow">
-            <img src={LeftArrowIcon} alt='right-arrow' />
-        </Typography>
-    );
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
 };
 
 const RightArrow = () => {
-    const {scrollNext} = useContext(VisibilityContext);
+  const { scrollNext } = useContext(VisibilityContext);
 
-    return (
-        <Typography onClick={() => scrollNext()} className="left-arrow">
-            <img src={RightArrowIcon} alt="right-arrow" />
-        </Typography>
-    )
-}
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
@@ -36,7 +37,15 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
           title={item.id || item}
           m="0 40px"
         >
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+          {isBodyParts ? (
+            <BodyPart
+              item={item}
+              bodyPart={bodyPart}
+              setBodyPart={setBodyPart}
+            />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}{" "}
         </Box>
       ))}
     </ScrollMenu>
--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -7,6 +7,15 @@ export const exerciseOptions = {
   }
   };
 
+  export const youtubeOptions = {
+    method: 'GET',
+    url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
+    headers: {
+      'X-RapidAPI-Key': '95dd9dd5e3msh172ef2555bdbab6p1b2527jsne788161337f6',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
